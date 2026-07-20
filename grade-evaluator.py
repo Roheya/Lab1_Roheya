78e723ee2d89cb373f270b9eabb4e095b1901126
root <r.touray@alustudent.com>
2026-07-20 10:22:14 -0700

    weight validation

--- grade-evaluator.py
+++ grade-evaluator.py
@@ -5,7 +5,7 @@ def grade_evaluator(filename):
     assignments = []
     try :
         with open(filename) as f:
-            lines = f.redlines()
+            lines = f.readlines()
     except FileNotFoundError:
         print("Error: File '{filename}' not found.")
         return
@@ -22,9 +22,39 @@ def grade_evaluator(filename):
             "assignment": name,
             "group": group,
             "score": score,
-            "weight":: weight
+            "weight": weight
         })
 # grade validation
-for row in assignments:
-    if score < 0 or score > 100:
-        print(f"Invalid score {score} in {row["assignment""])        
+
+    for row in assignments:
+        score = int(row["score"])
+        if score < 0 or score > 100:
+            print(f"Invalid score {score} in {row['assignment']}")
+
+#weight validation 
+    total_weight = sum(int(row["weight"]) for row in assignments)
+    if total_weight == 100:
+        print("the sum of the assignment weight is exactly 100")
+    else:
+        print("Error: the total weight must equal to 100")
+    
+
+    #Ensuring that the formative weight adds up to 60
+    formative_weight = 0
+    for row in assignments:
+        if row["group"] == "Formative":
+            weight = int(row["weight"])
+            formative_weight += weight
+    print(f"the formative assignment adds up to {formative_weight}")
+    
+    # Ensuring that the summative weight adds up to 40
+    summative_weight = 0
+    for row in assignments:
+        if row["group"] == "Summative":
+            weight = int(row["weight"])
+            summative_weight += weight
+    print(f"the summative assignments adds up to {summative_weight} exactly")
+
+
+
+grade_evaluator("grades.csv")
