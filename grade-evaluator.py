cc55bb7b84054604ffc193db7c8a9a5cdce885c3
root <r.touray@alustudent.com>
2026-07-21 15:10:56 -0700

    calucating the GPA and check if a student passed based on the percentage got on both categories

--- grade-evaluator.py
+++ grade-evaluator.py
@@ -24,14 +24,14 @@ def grade_evaluator(filename):
             "score": score,
             "weight": weight
         })
-# grade validation
+    # grade validation
 
     for row in assignments:
         score = int(row["score"])
         if score < 0 or score > 100:
             print(f"Invalid score {score} in {row['assignment']}")
 
-#weight validation 
+    #weight validation 
     total_weight = sum(int(row["weight"]) for row in assignments)
     if total_weight == 100:
         print("the sum of the assignment weight is exactly 100")
@@ -45,16 +45,66 @@ def grade_evaluator(filename):
         if row["group"] == "Formative":
             weight = int(row["weight"])
             formative_weight += weight
-    print(f"the formative assignment adds up to {formative_weight} exactly")
-    
+    if formative_weight == 60:
+        print("the formative assignments add up to exactly 60")
+    else:
+        print("Error: formative weight must equal 60")
     # Ensuring that the summative weight adds up to 40
     summative_weight = 0
     for row in assignments:
         if row["group"] == "Summative":
             weight = int(row["weight"])
             summative_weight += weight
-    print(f"the summative assignments adds up to {summative_weight} exactly")
+    if summative_weight == 40:
+        print("the summative assignment adds up to exactly 40")
+    else:
+        print("Error: summative grade must equal 40")
+    
+    # Calculating the GPA
+    sum_weighted_score = 0
+    for row in assignments:
+        score = int(row["score"])
+        weight = int(row["weight"])
+        weighted_score = score * weight
+        sum_weighted_score += weighted_score
+    
+    Total_Grade = (sum_weighted_score)/100
+    GPA = (Total_Grade/100)*5.0
+    
+    print(f"Total grade = {Total_Grade}")
+    print(f"the final GPA is {GPA}")
 
+    #Final decision(pass/fail)
+    #checking the percentage score for formative
+    formative_weighted_score = 0
+    formative_weight = 0
+    for row in assignments:
+        if row["group"] == "Formative":
+            score = int(row["score"])
+            weight = int(row["weight"])
+            formative_weighted_score += (score*weight)/100
+            formative_weight += weight
+    #percentage for formative
+    formative_percentage =(formative_weighted_score/formative_weight)*100
+    if formative_percentage >= 50:
+        print(f"Result: PASSED with a percentage of {formative_percentage}")
+    else:
+        print(f"Result: FAILED with a percentage of {formative_percentage}")
 
+    #checking percentage score for summative
+    summative_weighted_score = 0
+    summative_weight = 0
+    for row in assignments:
+        if row["group"] == "Summative":
+            score = int(row["score"])
+            weight = int(row["weight"])
+            summative_weighted_score += (score*weight)/100
+            summative_weight += weight
+    summative_percentage = (summative_weighted_score/summative_weight)*100
+    if summative_percentage >= 50:
+        print(f"Result: PASSED with a percentage of {summative_percentage}")
+    else:
+        print(f"Result: FAILED with a percentage of {summative_percentage}")
 
+    print(f"the student PASSED with a percentage of {summative_percentage} in the submmative and a percentage of {formative_percentage} in the formative")
 grade_evaluator("grades.csv")
